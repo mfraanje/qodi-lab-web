87339a1360a8467b448a527d44a1d3139a8fc6e4
mfraanje <150241076+mfraanje@users.noreply.github.com>
2024-12-17 14:18:45 +0100

    Update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,35 +2,21 @@
 import {
   Container,
   Group,
-  Title,
   Text,
-  Transition,
   Combobox,
   useCombobox,
   Image,
   ActionIcon,
-  Stack,
+  Button,
+  Grid,
 } from '@mantine/core';
 import classes from './Components.module.css';
-import { startTransition, useEffect, useState } from 'react';
+import { startTransition, useState } from 'react';
 import { Locale } from '@/i18n/config';
 import { setUserLocale } from '@/services/locale.service';
+import LogoComponent from './LogoComponent';
 
 export default function Header({ locale }: { locale: string }) {
-  const [switched, setSwitched] = useState(false);
-  const mainSubtitle = 'Software';
-  const [subtitle, setSubtitle] = useState(mainSubtitle);
-  const allSubtitles = [
-    'Development',
-    'Solutions',
-    'Consulting',
-    'Web',
-    'Platforms',
-    'Software',
-    'R&D',
-    'Data',
-  ];
-
   const languageOptions = [
     { language: 'nl', flag: 'images/nl-flag.svg' },
     { language: 'en', flag: 'images/en-flag.svg' },
@@ -39,41 +25,6 @@ export default function Header({ locale }: { locale: string }) {
     language: locale,
     flag: `images/${locale}-flag.svg`,
   });
-  useEffect(() => {
-    setSwitched(true);
-    subtitleTimer();
-  }, []);
-
-  let subtitleIndex = 1;
-
-  function subtitleTimer() {
-    const timer = setTimeout(async () => {
-      await rotateSubtitle();
-    }, 5000);
-    return () => clearTimeout(timer);
-  }
-
-  async function rotateSubtitle() {
-    if (subtitleIndex < allSubtitles.length) {
-      setSwitched(false);
-      await delay(400).then(() => {
-        setSubtitle(allSubtitles[subtitleIndex]);
-        setSwitched(true);
-        subtitleIndex++;
-        subtitleTimer();
-      });
-    } else {
-      setSwitched(false);
-      await delay(400).then(() => {
-        setSubtitle(mainSubtitle);
-        subtitleIndex = 0;
-        setSwitched(true);
-      });
-      rotateSubtitle();
-    }
-  }
-
-  const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
   const combobox = useCombobox({
     onDropdownClose: () => combobox.resetSelectedOption(),
@@ -92,79 +43,72 @@ export default function Header({ locale }: { locale: string }) {
     </Combobox.Option>
   ));
 
+  const scrollToBottom = () => {
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <div className={classes.header}>
       <Container size='xl' pos={'relative'}>
-        <Group py={'0.5rem'} justify='space-between'>
-          <Group gap={0}>
-            <Image
-              src='/images/qodisoft.svg'
-              h={30}
-              pr={'0.5rem'}
-              mt={'0.2rem'}
-            ></Image>
-            <Title className={classes.logoText} order={4} mt={'0.15rem'}>
-              <Stack gap={0}>
-                <Group gap={0}>
-                  <span className={classes.qodi}>Qodi</span>
-                  <span className={classes.slash}>/</span>
-                  <Transition
-                    mounted={switched}
-                    transition='fade-down'
-                    duration={400}
-                    timingFunction='ease'
-                  >
-                    {(styles) => (
-                      <div style={styles}>
-                        <span className={classes.subtitle}>{subtitle}</span>
-                      </div>
-                    )}
-                  </Transition>
-                </Group>
-                <span className={classes.slogan}>Oplossingen op maat</span>
-              </Stack>
-            </Title>
-          </Group>
-          <Group justify='flex-end'>
-            <Text
-              mt={'0.3rem'}
-              size='sm'
-              fw={500}
-              visibleFrom='xs'
-              className={classes.headerText}
-            >
-              Software Development | Consulting
-            </Text>
-            <Combobox
-              store={combobox}
-              width={55}
-              position='bottom'
-              withArrow
-              onOptionSubmit={(val) => {
-                setSelectedLanguage({
-                  language: val,
-                  flag: `images/${val}-flag.svg`,
-                });
-                onChangeLanguage(val);
-                combobox.closeDropdown();
-              }}
-            >
-              <Combobox.Target>
-                <ActionIcon
-                  mt={5}
-                  variant='subtle'
-                  onClick={() => combobox.toggleDropdown()}
-                >
-                  <Image src={selectedLanguage.flag}></Image>
-                </ActionIcon>
-              </Combobox.Target>
+        <Grid py={'0.5rem'}>
+          <Grid.Col span={{ base: 12, sm: 4 }}>
+            <LogoComponent></LogoComponent>
+          </Grid.Col>
 
-              <Combobox.Dropdown>
-                <Combobox.Options>{options}</Combobox.Options>
-              </Combobox.Dropdown>
-            </Combobox>
-          </Group>
-        </Group>
+          <Grid.Col span={{ base: 12, sm: 8 }}>
+            <Group justify='flex-end' mt={5}>
+              {/* <Text
+                mt={'0.3rem'}
+                size='sm'
+                fw={500}
+                visibleFrom='xs'
+                className={classes.headerText}
+              >
+                Software Development | Consulting
+              </Text> */}
+
+              <Combobox
+                store={combobox}
+                width={55}
+                position='bottom'
+                withArrow
+                onOptionSubmit={(val) => {
+                  setSelectedLanguage({
+                    language: val,
+                    flag: `images/${val}-flag.svg`,
+                  });
+                  onChangeLanguage(val);
+                  combobox.closeDropdown();
+                }}
+              >
+                <Combobox.Target>
+                  <ActionIcon
+                    variant='subtle'
+                    onClick={() => combobox.toggleDropdown()}
+                  >
+                    <Image src={selectedLanguage.flag}></Image>
+                  </ActionIcon>
+                </Combobox.Target>
+
+                <Combobox.Dropdown>
+                  <Combobox.Options>{options}</Combobox.Options>
+                </Combobox.Dropdown>
+              </Combobox>
+              {/* <Button variant='subtle' size='xs' c={'blue.9'}>
+                Kosten
+              </Button>
+              <Button variant='subtle' size='xs' c={'blue.9'}>
+                Over ons
+              </Button> */}
+              <Button variant='outline' size='xs' onClick={scrollToBottom}>
+                Contact
+              </Button>
+            </Group>
+          </Grid.Col>
+        </Grid>
       </Container>
     </div>
   );
---
+++ src/components/LogoComponent.tsx
@@ -0,0 +1,93 @@
+import { Image, Group, Title, Stack, Transition } from '@mantine/core';
+import classes from './Components.module.css';
+import { useEffect, useState } from 'react';
+export default function LogoComponent() {
+  const [switched, setSwitched] = useState(false);
+  const mainSubtitle = 'Software';
+  const [subtitle, setSubtitle] = useState(mainSubtitle);
+  const allSubtitles = [
+    'Development',
+    'Solutions',
+    'Consulting',
+    'Web',
+    'Platforms',
+    'Software',
+    'R&D',
+    'Data',
+  ];
+
+  useEffect(() => {
+    setSwitched(true);
+    subtitleTimer();
+  }, []);
+
+  let subtitleIndex = 1;
+
+  function subtitleTimer() {
+    const timer = setTimeout(async () => {
+      await rotateSubtitle();
+    }, 5000);
+    return () => clearTimeout(timer);
+  }
+
+  async function rotateSubtitle() {
+    if (subtitleIndex < allSubtitles.length) {
+      setSwitched(false);
+      await delay(400).then(() => {
+        setSubtitle(allSubtitles[subtitleIndex]);
+        setSwitched(true);
+        subtitleIndex++;
+        subtitleTimer();
+      });
+    } else {
+      setSwitched(false);
+      await delay(400).then(() => {
+        setSubtitle(mainSubtitle);
+        subtitleIndex = 0;
+        setSwitched(true);
+      });
+      rotateSubtitle();
+    }
+  }
+
+  const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <Group gap={0} onClick={scrollToTop} className={classes.logo}>
+      <Image
+        src='/images/qodisoft.svg'
+        h={30}
+        pr={'0.5rem'}
+        mt={'0.2rem'}
+      ></Image>
+      <Title className={classes.logoText} order={4} mt={'0.15rem'}>
+        <Stack gap={0}>
+          <Group gap={0}>
+            <span className={classes.qodi}>Qodi</span>
+            <span className={classes.slash}>/</span>
+            <Transition
+              mounted={switched}
+              transition='fade-down'
+              duration={400}
+              timingFunction='ease'
+            >
+              {(styles) => (
+                <div style={styles}>
+                  <span className={classes.subtitle}>{subtitle}</span>
+                </div>
+              )}
+            </Transition>
+          </Group>
+          <span className={classes.slogan}>Oplossingen op maat</span>
+        </Stack>
+      </Title>
+    </Group>
+  );
+}
